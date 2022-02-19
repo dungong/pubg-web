b126f9bbc6b4d4feef7d9c647876296673b0fb9d
yuhyun <dbgus101@gmail.com>
2022-02-19 18:16:05 +0900

    make utill functions

---
+++ src/utils/functions.ts
@@ -0,0 +1,48 @@
+export function debounce(func: Function, wait: number, leading = false) {
+  let inDebounce;
+  return function () {
+    const context = this;
+    let callNow = leading && !inDebounce;
+
+    const later = () => {
+      inDebounce = null;
+      if (!leading) func.apply(context, arguments);
+    };
+
+    clearTimeout(inDebounce);
+    inDebounce = setTimeout(later, wait);
+
+    if (callNow) func.apply(context, arguments);
+  };
+}
+
+export function throttle(callback: Function, waitUntil: number) {
+  let timer;
+  return function () {
+    if (!timer) {
+      timer = setTimeout(() => {
+        callback.apply(this, arguments);
+        timer = null;
+      }, waitUntil);
+    }
+  };
+}
+
+export function clickOutside(
+  node: HTMLElement,
+  handler: () => void
+): { destroy: () => void } {
+  const onClick = (event: MouseEvent) =>
+    node &&
+    !node.contains(event.target as HTMLElement) &&
+    !event.defaultPrevented &&
+    handler();
+
+  document.addEventListener("click", onClick, true);
+
+  return {
+    destroy() {
+      document.removeEventListener("click", onClick, true);
+    },
+  };
+}
