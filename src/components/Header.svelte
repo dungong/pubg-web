264bab383fdce007b4b2d815d3a8050ead83117e
yuhyun <dbgus101@gmail.com>
2022-02-19 18:16:20 +0900

    make page header

---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+  import PgSearch from "./PgSearch.svelte";
+  const tabs = ["홈", "랭킹", "커뮤니티"];
+</script>
+
+<div class="wrapper">
+  <ul>
+    {#each tabs as tab}
+      <li>{tab}</li>
+    {/each}
+  </ul>
+  <PgSearch defaultValue="abc" />
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 86px;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    gap: 32px;
+  }
+  input {
+    border: none;
+    outline: none;
+  }
+</style>
