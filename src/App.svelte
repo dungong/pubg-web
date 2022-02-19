57e12adeeb63cf6cb315eb5e090142b18b2111f1
yuhyun <dbgus101@gmail.com>
2022-02-19 18:17:20 +0900

    fix pg title and main pg stycle

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
 <script lang="ts">
-	export let name: string;
+  import Header from "./components/Header.svelte";
+
+  export let title: string;
 </script>
 
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+<svelte:body />
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Header />
+  <p />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    text-align: center;
+    padding: 0;
+    margin: 0 auto;
+  }
 </style>
