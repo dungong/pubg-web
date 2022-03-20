d2ffb550fd345f5854140968720e58cf776c6273
yuhyun <dbgus101@gmail.com>
2022-03-20 18:26:43 +0900

    setup urql

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import Router from "svelte-spa-router";
   import routes from "./routers";
+  import { initClient } from "@urql/svelte";
+  initClient({
+    url: "https://graphql-pokeapi.graphcdn.app/",
+  });
 </script>
 
 <Router {routes} />
---
+++ src/urql/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { createClient } from "@urql/svelte";
+  import { setClient } from "@urql/svelte";
+
+  const client = createClient({
+    url: "https://graphql-pokeapi.graphcdn.app/",
+  });
+
+  setClient(client);
+</script>
